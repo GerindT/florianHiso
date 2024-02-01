2988de88503cdbc6a24535c3c4e6bb260a6b1c82
GerindT <gerindt@gmail.com>
2024-02-01 23:49:01 +0100

    hero section

--- florian-hiso/src/App.tsx
+++ florian-hiso/src/App.tsx
@@ -25,6 +25,30 @@ function App() {
     <>
       <div>
         <MainNavbar />
+        <div
+          className="hero min-h-[89vh] bg-base-200 mb-[2em] rounded-xl "
+          style={{
+            backgroundImage:
+              "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+          }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">
+                Hello there, General Kenoby
+              </h1>
+              <p className="mb-5  ">
+                Are you looking for the best place to find information related
+                to computer science? You are in the right place. <br /> Do you
+                need useless information? You are in the right place. <br /> Do
+                you need to waste your time? You are in the right place. <br />
+                We are here to help you.
+              </p>
+              <button className="btn btn-primary">Come Child</button>
+            </div>
+          </div>
+        </div>
 
         <div className=" grid grid-cols-1  md:grid-cols-2 gap-[2em] mb-[2em]">
           <Posts />
