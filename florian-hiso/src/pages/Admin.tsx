6842998e5e241537c349eca5c54ccbac7b7b1096
GerindT <gerindt@gmail.com>
2024-02-03 20:02:29 +0100

    admin

--- florian-hiso/src/pages/Admin.tsx
+++ florian-hiso/src/pages/Admin.tsx
@@ -1,3 +1,55 @@
 export default function Admin() {
-  return <div>Admin</div>;
+  return (
+    <div className="w-full h-[90vh]  flex justify-center items-center">
+      <div className="card w-96 bg-base-200 shadow-xl mx-[1em] ">
+        <div className="card-body">
+          <h2 className="card-title  text-primary">
+            Login into the Admin Dashboard
+          </h2>
+
+          <form className="max-w-md ">
+            <div className="relative z-0 w-full mb-5 group">
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="block py-2.5 px-0 w-full text-sm text-primary bg-transparent border-0 border-b-2 border-primary appearance-none  focus:outline-none focus:ring-0 focus:border-primary peer"
+                placeholder=" "
+                required
+              />
+              <label
+                for="name"
+                className="peer-focus:font-medium absolute text-sm text-primary  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-primary  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Name
+              </label>
+            </div>
+            <div className="relative z-0 w-full mb-5 group">
+              <input
+                type="password"
+                name="floating_password"
+                id="floating_password"
+                className="block py-2.5 px-0 w-full text-sm text-primary bg-transparent border-0 border-b-2 border-primary appearance-none  focus:outline-none focus:ring-0 focus:border-primary peer"
+                placeholder=" "
+                required
+              />
+              <label
+                for="floating_password"
+                className="peer-focus:font-medium absolute text-sm text-primary  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-primary  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Password
+              </label>
+            </div>
+
+            <button
+              type="submit"
+              className="text-white bg-primary hover:opacity-85 focus:ring-4 focus:outline-none focus:ring-base-200 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center text-base-100 "
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
