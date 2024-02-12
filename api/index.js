395bace6a88ea8162dd5b7559d9d4bac6aea2013
GerindT <gerindt@gmail.com>
2024-02-12 17:47:04 +0100

    a

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors"); // Import the cors middleware
 const app = express();
-const port = 3000;
+const port = 3000 || process.env.PORT;
 
 const db = require("./queries");
 
